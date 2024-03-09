1df6dd328d70fc94c9616a98cf43903a207b9109
Luisa Boutin <lboutin23@gmail.com>
2024-03-09 13:44:19 -0300

    Minor theme adjustments

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -9,28 +9,10 @@ const Footer = () => {
           <a
             href="/"
             className="mb-3 me-2 mb-md-0 text-body-secondary text-decoration-none lh-1">
-            <i className="footer-style bi bi-bootstrap"></i>
+            <i className="footer-style bi bi bi-code-slash"></i>
           </a>
           <span className="footer-style mb-3 mb-md-0">&nbsp; Luisa Boutin</span>
         </div>
-
-        <ul className="nav col-md-4 justify-content-end list-unstyled d-flex">
-          <li className="ms-3">
-            <a className="footer-style text-body-secondary" href="/">
-              <i className="footer-style bi bi-twitter mx-2"></i>
-            </a>
-          </li>
-          <li className="ms-3">
-            <a className="footer-style text-body-secondary" href="/">
-              <i className="footer-style bi bi-instagram mx-2"></i>
-            </a>
-          </li>
-          <li className="ms-3">
-            <a className="text-body-secondary" href="/">
-              <i className="footer-style bi bi-facebook mx-2"></i>
-            </a>
-          </li>
-        </ul>
       </footer>
     </div>
   );
