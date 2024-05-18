d2f6b5344c84443488eba3c6af95ed957a95a0fb
Luisa Boutin <lboutin23@gmail.com>
2024-05-17 23:40:02 -0300

    [refactor] moving LatexRenderer code to Home

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
-import LatexRenderer from "../LatexRenderer/LatexRenderer";
 import { tokenize } from "../Parser/Tokenizer";
 import { convertTokensToLatex } from "../Parser/Translator";
 import "./Home.css";
+import "katex/dist/katex.min.css";
+import Latex from "react-latex";
 
 const Home = () => {
   const [translatedLatex, setLatexCode] = useState("");
@@ -20,6 +21,8 @@ const Home = () => {
     const translatedLatex = convertTokensToLatex(tokens);
     setLatexCode(translatedLatex);
   };
+
+  const wrappedLatexCode = `$$${translatedLatex}$$`;
 
   return (
     <div>
@@ -55,7 +58,54 @@ const Home = () => {
           </div>
         </div>
       </div>
-      <LatexRenderer mathematicaCode={input} latexCode={translatedLatex} />
+      <div className="container-fluid">
+        <div className="card-deck mb-3 text-center">
+          <div id="wolframDisplay" className="card mb-4 col">
+            <div className="card-header">
+              <h4 className="my-0 font-weight-normal">Input em Wolfram:</h4>
+            </div>
+            <div className="card-body">
+              <h1 className="card-title pricing-card-title">
+                <p className="lead">{input}</p>
+              </h1>
+            </div>
+          </div>
+          <div id="latexOutput" className="card mb-4 col">
+            <div className="card-header">
+              <h4 className="my-0 font-weight-normal">Output em LaTeX:</h4>
+            </div>
+            <div className="card-body">
+              <h1 className="card-title pricing-card-title">
+                <p className="lead">{translatedLatex}</p>
+              </h1>
+            </div>
+          </div>
+          <div id="latexRender" className="card mb-4 col">
+            <div className="card-header">
+              <h4 className="my-0 font-weight-normal">LaTeX Renderizado:</h4>
+            </div>
+            <div className="card-body">
+              <h1 className="card-title pricing-card-title">
+                <p className="lead">
+                  <Latex>{wrappedLatexCode}</Latex>
+                </p>
+              </h1>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div id="tokenOutput" className="card mb-4 col">
+        <div className="card-header">
+          <h4 className="my-0 font-weight-normal text-center">
+            Input tokenizado:
+          </h4>
+        </div>
+        <div className="card-body">
+          <h1 className="card-title pricing-card-title">
+            <pre>{JSON.stringify(tokens, null, 2)}</pre>
+          </h1>
+        </div>
+      </div>
     </div>
   );
 };
